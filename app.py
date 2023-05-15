5a9db2bf805f04579d7f08d8fea8491738ff7588
grassLEE <jeff.graessley@gmail.com>
2023-05-15 07:05:05 -0700

    init

---
+++ app.py
@@ -0,0 +1,32 @@
+import os
+from datetime import datetime
+
+class Notes:
+    def __init__(self, name):
+        self.name = name
+        
+    def newEntry(name):
+        name = name
+        pa_filename = input("Enter the PA Filename\n").lower()
+        value = os.fspath(pa_filename)  # os path like object, accepts variable value
+        with open(value, "+a", encoding="UTF-8") as new: # with statement to open txt files as 'new'
+            content = str(input())
+            new.write("\n")
+            new.write(content)
+            now = datetime.now() # variable 'now' with the value of current date/time
+            new_form = now.strftime("%Y-%b: %a %d | %I:%M%p") # .strftime allows formatting "%b" is the symbol for rendering the month in abbreviated text for example, Jun for June. 
+            new.write("\n")
+            new.write(new_form)
+            new.write("\n")
+            new.close()
+
+CT = Notes("Carlos")
+CG = Notes("Christian")
+SS = Notes("Santiago")
+CC = Notes("Ceaser")
+BO = Notes("Bo")
+MM = Notes("Michael")
+New = Notes("Placeholder")
+New.newEntry()
+
+
---
+++ reader.py
@@ -0,0 +1,21 @@
+import os
+
+
+class Reader():
+    def review(name):
+        name = name
+        new_value = input("Enter file name:\n")
+        updated_value = os.fspath(new_value)
+        with open(updated_value, encoding='utf-8') as f:
+            read_data = f.read()
+            print(read_data)
+            f.close
+
+bo = Reader()
+carlos = Reader()
+ceaser = Reader()
+christian = Reader()
+michael = Reader()
+santiago = Reader()
+new = Reader()
+new.review()
